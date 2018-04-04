5b0d5fcd776f9424eaf33e8b9b2ff1cfbe64ebfe
Anton Kramarev <kramarev.anton@gmail.com>
2018-04-04 23:13:19 +0300

    Initial commit

---
+++ vendor/github.com/dakaraj/romanserver/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/dakaraj/romannumerals"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		urlParameters := strings.Split(r.URL.Path, "/")
+		if urlParameters[1] == "roman_number" {
+			number, _ := strconv.Atoi(urlParameters[2])
+			if number == 0 || number > 10 {
+				w.WriteHeader(http.StatusNotFound)
+				w.Write([]byte("404 - Not Found"))
+			} else {
+				fmt.Fprintf(w, "%q", html.EscapeString(romannumerals.Numbers[number]))
+			}
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("400 - Bad Request"))
+		}
+	})
+
+	s := &http.Server{
+		Addr:           ":8000",
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
+}
