4789ae31e9e10aabe574e5dece57571fc0bcd243
Anton Kramarev <ankra@ciklum.com>
2018-08-13 12:07:41 +0300

    Working pipeline fasthttp client

--- fasthttp-test/main.go
+++ fasthttp-test/main.go
@@ -7,19 +7,33 @@ import (
 	f "github.com/valyala/fasthttp"
 )
 
-var client = f.PipelineClient{
-	// Name:         "BandStack/Golang/HTTPClient/email:kramarev.anton@gmail.com",
+var wcfmcClient = f.PipelineClient{
+	Addr:         "www.whocanfixmycar.com",
+	IsTLS:        true,
 	ReadTimeout:  10 * time.Second,
 	WriteTimeout: 10 * time.Second,
 }
 
+// var wcfmcClient = f.Client{
+// 	ReadTimeout:  10 * time.Second,
+// 	WriteTimeout: 10 * time.Second,
+// }
+
 func main() {
 	fmt.Println("Starting an HTTP request")
 	req := f.AcquireRequest()
-	req.SetRequestURI("https://whocanfixmycar.com")
-	req.Header.Add("Custom", "Header")
-	client.Do(req, resp)
+	res := f.AcquireResponse()
+
+	req.SetRequestURI("https://www.whocanfixmycar.com")
+	req.Header.SetMethod("GET")
+	req.Header.Add("Accept", "application/json")
+
+	wcfmcClient.Do(req, res)
+
+	sCode := res.StatusCode()
+	bodyBytes := res.Body()
+	headers := res.Header.String()
 
 	// sCode, body, _ := client.Get(nil, "https://whocanfixmycar.com")
-	fmt.Printf("Code: %d\nbody: %s\n", sCode, string(body[:100]))
+	fmt.Printf("Code: %d\nheaders: %s\nbody: %s\n", sCode, headers, string(bodyBytes))
 }
